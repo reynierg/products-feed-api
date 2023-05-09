31b786f3231d1c9d800cfa24e36e786562cb90cf
Reynier Garcia <reynierg@protonmail.com>
2023-05-09 21:18:49 +0200

    style: address flake8 reported issues

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
